350d0c25c2d92b0124e2c8b54d210b141ff56bff
Norbert Czarnota <czarny-s@o2.pl>
2023-05-04 17:51:32 +0200

    Initial commit, books project, get operations

---
+++ books.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+app = FastAPI()
+
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
+
+@app.get('/books/my_book')
+async def read_my_book():
+    return {'book_title': 'My favourite book!'}
+
+@app.get('/books/{book_title}')
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get('/books/{book_author}/')
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+        book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
